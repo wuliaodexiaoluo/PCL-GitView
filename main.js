87ff18aab5cb1f44aa5cf4d9654075c706e98802
深黑森林 <131975043+Deep-Dark-Forest@users.noreply.github.com>
2024-04-04 20:28:08 +0800

    这么改会不会炸？

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function fetchGitHubIssues(callback) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.github.com/repos/Hex-Dragon/PCL2/issues');
+  xhr.open('GET', 'https://api.github.com/repos/Hex-Dragon/PCL2/issues?q=is%3Aissue+');
   xhr.onload = function() {
     if (xhr.status === 200) {
       const issues = JSON.parse(xhr.responseText);
